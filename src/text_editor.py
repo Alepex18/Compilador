e1f05df510b2c8ee1c0408bc68c32d2a5ca72c38
Alepex18 <ale.arteaga.18.09@gmail.com>
2024-01-25 15:02:00 -0400

    fix: changed the way the program closes tabs and implemented New File option in the File menu

--- src/text_editor.py
+++ src/text_editor.py
@@ -3,13 +3,12 @@ import PySimpleGUI as sg
 sg.theme('DarkPurple7')  
 
 def ltab(i=0):
-   return [[sg.Multiline(default_text='Prueba',disabled=False, size=(800,33.5),key=f'-multline{i}-')]]
+   return [[sg.Multiline(default_text='',disabled=False, size=(800,33.5),key=f'-multline{i}-')]]
 def main_window():
     tabgl = [[sg.Tab('New File',ltab(),key='0')]]
     tabg = sg.TabGroup(tabgl,key='-tabs-')
-    layout = [[sg.Menu([['File', ['Open','Close']], ['Edit', ['Edit Me', ]]],  k='-CUST MENUBAR-',p=0)],
-            [tabg],
-            [sg.Button('Add Tab',key='-add-')]]
+    layout = [[sg.Menu([['File', ['New File','Open','Close']], ['Edit', ['Edit Me', ]]],  k='-CUST MENUBAR-',p=0)],
+            [tabg]]
     i = 1
     
     # Create the Window
@@ -20,7 +19,7 @@ def main_window():
         event, values = window.read()
         if event == sg.WIN_CLOSED: # if user closes window
             break
-        if event == '-add-': # if user clicks Add Tab
+        if event == 'New File': # if user clicks Add Tab
             tabg.add_tab(sg.Tab(f'New File {i}',ltab(i),key=i))
             window[i].select()
             i += 1
@@ -34,12 +33,15 @@ def main_window():
                 with open(filename,'r') as file:
                     text = file.read()
                 
+
                 ctab = window['-tabs-'].get()
                 print(ctab)
                 window[ctab].update(title=tabname)
                 window[f'-multline{ctab}-'].update(text)
         if event == 'Close': # if user clicks Close
-            #get the index of the tab that is selected
+            # Code to actually delete the tab (isn't working well)
+            
+            """ #get the index of the tab that is selected
             index = window["-tabs-"].TKNotebook.index('current')
             #remove the selected tab from the TabGroup.TKNotebook
             #TabGroup.TkNotebook is part of the structure for how a TabGroup is defined
@@ -48,8 +50,10 @@ def main_window():
             window["-tabs-"].Rows.pop(index)
             #Update the TabCount(part of the structure of TabGroups)
             window["-tabs-"].TabCount -= 1
-
-            window.refresh
+            window.refresh """
+            
+            # Code that makes the current tab invisible
+            window[values['-tabs-']].update('',visible=False)
     window.close()
 
 main_window()
