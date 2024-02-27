437798117abe23fa14d066ac029ab1a968e9c713
Abraham Galue <abrahamgalue512@gmail.com>
2024-02-27 09:27:59 -0400

    refactor: get_characters function to remove zero-width spaces

--- src/utils/get_characters.py
+++ src/utils/get_characters.py
@@ -2,23 +2,24 @@ import re
 
 def get_characters(str):
   str_without_spaces = re.sub(r'\s', '', str)
+  str_without_zero_width_space = re.sub(r'\u200b', '', str_without_spaces)
     
-  numbers = re.findall(r"\d", str_without_spaces)
-  letters = re.findall(r"[a-zA-Z]", str_without_spaces)
-  special_characters = re.findall(r"[^a-zA-Z0-9]", str_without_spaces)
+  numbers = re.findall(r"\d", str_without_zero_width_space)
+  letters = re.findall(r"[a-zA-Z]", str_without_zero_width_space)
+  special_characters = re.findall(r"[^a-zA-Z0-9]", str_without_zero_width_space)
 
   characters = {
     'numbers': {
-      'list': numbers,
       'count': len(numbers),
+      'list': numbers,
     },
     'letters': {
-      'list': letters,
       'count': len(letters),
+      'list': letters,
     },
     'special_characters': {
-      'list': special_characters,
       'count': len(special_characters),
+      'list': special_characters,
     },
   }
 
