fb75a556a5e3a352011fa010d2e1b26c3000dd42
Alepex18 <ale.arteaga.18.09@gmail.com>
2024-04-16 11:47:55 -0400

    feature: lexic changed to sintax and added sintax_analysis function

---
+++ src/sintax.py
@@ -0,0 +1,40 @@
+from lark import Lark
+
+def sintax_analisis(expression):
+    grammar = r"""
+        start: expr
+        ?expr: IF expr COMP expr THEN expr ELSE expr
+            | FALSE
+            | STRING
+            | ID 
+            | WHILE
+            | NUMBER
+            | TRUE
+            | FOR
+            | IF
+            |ID ASSING expr 
+            |expr (OP)* expr
+            |expr (COMP)* expr
+        IF: "if"
+        OP: "+" | "-" | "*" | "/"
+        ASSING: "="
+        THEN: ":"
+        COMP: "==" | "!=" | "<" | ">" | "<=" | ">="
+        ELSE: "else"
+        FALSE: "false"
+        STRING: /"[^"]*"/
+        ID: /[a-zA-Z_]\w*/
+        WHILE: "while"
+        NUMBER: /\d+(\.\d+)?/ 
+        TRUE: "true"
+        FOR: "for"
+        %ignore " "
+    """
+
+    parser = Lark(grammar)
+
+    #expression = "if x == 5: a else y"
+    tree = parser.parse(expression)
+
+    return (tree,tree.pretty())
+print(sintax_analisis("if a > 2: b = 2*2 else c")[1])
